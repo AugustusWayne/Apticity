e804227e37b0f428a3521991c56aea6eff04ac17
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2024-11-16 16:03:04 +0530

    project-setup

---
+++ apticity-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WalletConnect from './components/walletConnect';
+
+function App() {
+  return (
+    <>
+    <WalletConnect/>
+    </>
+  );
+}
+
+export default App;
